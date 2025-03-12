19fb7d4d8fc2e3bdf14d56833af68e677245c1ad
KBOlifant <241299@virtualwindow.co.za>
2025-03-12 14:22:38 +0200

    Card Section and web setup

--- reactvisuals/src/App.js
+++ reactvisuals/src/App.js
@@ -1,11 +1,38 @@
 import logo from './logo.svg';
 import './App.css';
+import React from 'react';
+import Bootstrap from 'bootstrap';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Cards from './components/Cards';
 
 function App() {
   return (
     <div className="App">
-      <section className="Hero">
-        <h1>Hero</h1>
+      <section className="HeroSection">
+        <h1 className='text-warning'>Hero</h1>
+      </section>
+
+      <section className="cardSection">
+        <div className='card-Section-Title'>
+          <h2>Select data</h2>
+          <div className='cards'>
+            <Cards />
+          </div>
+        </div>
+      </section>
+
+      <section className="dataAnalysisSection text-white">
+        <div className='data-Section-Title'>
+          <h2>Selected Data analysis</h2>
+          <h4 className='pt-4'>some more text</h4>
+        </div>
+        
+        <div className="bigGraph">
+
+        </div>
+        <div className="smallGraph">
+
+        </div>
       </section>
     </div>
   );
